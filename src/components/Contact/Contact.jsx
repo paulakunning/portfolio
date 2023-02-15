0f3e7e97d13b1cb67c2a648f07778215d56a4813
Paula Kunning <paulakunning@gmail.com>
2023-02-15 18:06:36 -0300

    assets moved

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,28 +12,28 @@ export default function Contact() {
         <h2>{t("contact.subtitle1")}</h2>
         <a href="https://www.linkedin.com/in/paula-kunning/">
           <img
-            src="/src/assets/linkedin-footer.png"
+            src="/src/components/assets/linkedin-footer.png"
             width="70px"
             alt="linkedin icon"
           />
         </a>
         <a href="https://github.com/paulakunning">
           <img
-            src="/src/assets/github-footer.png"
+            src="/src/components/assets/github-footer.png"
             width="70px"
             alt="github icon"
           />
         </a>
         <a href="https://twitter.com/pau_dev_">
           <img
-            src="/src/assets/twitter-footer.png"
+            src="/src/components/assets/twitter-footer.png"
             width="70px"
             alt="twitter icon"
           />
         </a>
       </div>
       <div className="contact-container">
-        <img src="/src/assets/gmail.png" width="70px" alt="email icon" />
+        <img src="/src/components/assets/gmail.png" width="70px" alt="email icon" />
         <h2>
           {t("contact.subtitle2")}{" "}
           <a href="mailto:paulakunning@gmail.com"> email</a>!
