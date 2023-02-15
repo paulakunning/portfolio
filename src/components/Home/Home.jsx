a4f32d48a42724c0773851e1a6749461f424a162
Paula Kunning <paulakunning@gmail.com>
2023-02-15 18:09:36 -0300

    images changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <h2>{t("home.subtitle")}</h2>
         <div className="icons">
           <a href="https://www.linkedin.com/in/paula-kunning/" target="blank">
-            <img src="./src/assets/linkedin.png" alt="linkedin icon" width="70px" />
+            <img src="https://drive.google.com/file/d/1Twda6VYCmhyrgU9YiQLAdqAMg2jJ-h6a/view?usp=share_link" alt="linkedin icon" width="70px" />
           </a>
           <a href="https://github.com/paulakunning" target="blank">
             <img src="/src/assets/github.png" alt="github icon" width="70px" />
